57bb88611d24e875d67f02b5b246496855c64cc3
Walter Hjelmar <walter@hjelmar.com>
2025-03-02 16:44:48 -0500

    fix: removing unneeded boilerpoint code

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
-import plugins from './plugins';
-import { executeHooks } from './hooks';
-
-// Load plugins
-plugins.forEach(plugin => plugin());
-
-// Execute prestart hooks
-executeHooks('prestart');
-
-// Start the application
-console.log('Starting the application...');
-
-// Execute poststart hooks
-executeHooks('poststart');
+const events = ical.sync.parseFile('example-calendar.ics');
+// loop through events and log them
+for (const event of Object.values(events)) {
+    console.log(
+        'Summary: ' + event.summary +
+        '\nDescription: ' + event.description +
+        '\nStart Date: ' + event.start.toISOString() +
+        '\n'
+    );
+};
